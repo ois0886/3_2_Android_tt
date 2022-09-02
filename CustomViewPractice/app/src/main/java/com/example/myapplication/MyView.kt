47679c09e601a570bee837f2bc0f7d9b24867547
oh in seong <superohinsung@naver.com>
2022-09-02 15:42:19 +0900

    customView 실습

---
+++ CustomViewPractice/app/src/main/java/com/example/myapplication/MyView.kt
@@ -0,0 +1,73 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import kotlin.random.Random
+
+class MyView : View {
+    var rect = Rect(10, 10, 110, 110)
+    var circleX = 100F
+    var circleY = 100F
+    var circleR = 50F
+
+    var triangleX = 100F
+    var triangleY = 100F
+    var triangleW = 100F
+    var color = Color.BLUE
+    private var paint = Paint()
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        paint.color = color
+
+        var random = Random.nextInt(3) + 1  //난수생성
+
+        when (random) {
+            1 -> {  //1일경우 사각형
+                canvas.drawRect(rect, paint)
+            }
+            2 -> {   //2일경후 원
+                canvas.drawCircle(circleX, circleY, circleR, paint)
+            }
+            3 -> {   //3일경우 삼각형
+                var hw = triangleW / 2
+                var path = Path()
+                path.moveTo(triangleX, triangleY - hw)
+                path.lineTo(triangleX - hw, triangleY + hw)
+                path.lineTo(triangleX + hw, triangleY + hw)
+                path.lineTo(triangleX, triangleY - hw)
+                path.close()
+                canvas.drawPath(path, paint)
+            }
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+
+        if (event.action == MotionEvent.ACTION_DOWN ||
+            event.action == MotionEvent.ACTION_MOVE
+        ) {
+            rect.left = event.x.toInt()
+            rect.top = event.y.toInt()
+            rect.right = rect.left + 100
+            rect.bottom = rect.top + 100
+
+            circleX = event.x
+            circleY = event.y
+
+            triangleX = event.x
+            triangleY = event.y
+
+            invalidate()
+
+            return true
+        }
+        return super.onTouchEvent(event)
+    }
+}
