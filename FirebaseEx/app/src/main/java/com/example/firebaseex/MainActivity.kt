a4c2e0e0c88f65a2eba8e615e5f8e27eff84d1ed
oh in seong <superohinsung@naver.com>
2022-11-05 15:13:21 +0900

    파이어 베이스 9주차 실습

---
+++ FirebaseEx/app/src/main/java/com/example/firebaseex/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.firebaseex
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firebaseex.databinding.ActivityMainBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (Firebase.auth.currentUser == null) {
+            startActivity(
+                Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
+        binding.textUID.text = Firebase.auth.currentUser?.uid ?: "No User"
+
+        binding.buttonSignout.setOnClickListener {
+            Firebase.auth.signOut()
+            finish()
+        }
+    }
+}
